fd267b635ed307a819841c3d5710a60a967747b7
Pierre Zhang <chromasetha@outlook.com>
2024-03-14 22:05:21 +0800

    Update drag_and_drop.bat

--- drag_and_drop.bat
+++ drag_and_drop.bat
@@ -4,16 +4,10 @@ set "script_path=%~dp0process_zip.py"
 :loop
 if "%~1"=="" goto :eof
 python "%script_path%" "%~1"
-echo Processing complete. Press any key to exit...
+echo DONE! Press any key to exit...
 pause > nul
 
-:: Emulate Alt+Tab
-:: sendkeys.bat
-echo Set WshShell = WScript.CreateObject("WScript.Shell") > sendkeys.vbs
-echo WshShell.SendKeys "%%{TAB}" >> sendkeys.vbs
-echo WshShell.SendKeys "%%{TAB}" >> sendkeys.vbs
-cscript /nologo sendkeys.vbs
-del sendkeys.vbs
+start "" "%~dp0"
 
 shift
 goto loop
