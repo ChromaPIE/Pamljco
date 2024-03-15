7b72bb537b8d432c93df92d2769aeccb21b62353
Pierre Zhang <chromasetha@outlook.com>
2024-03-15 14:37:28 +0800

    Update drag_and_drop.bat

--- drag_and_drop.bat
+++ drag_and_drop.bat
@@ -4,10 +4,7 @@ set "script_path=%~dp0process_zip.py"
 :loop
 if "%~1"=="" goto :eof
 python "%script_path%" "%~1"
-echo DONE! Press any key to exit...
+echo Processing complete. Press any key to exit...
 pause > nul
-
-start "" "%~dp0converted\scracherry\assets"
-
 shift
 goto loop
